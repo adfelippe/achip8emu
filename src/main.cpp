bd8f0ba76120211f8c4c9a12607709fdee8b74d5
Anderson Felippe <adfelippe@gmail.com>
2023-06-19 20:46:04 +0200

    Pass program file path as argument to executable

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include "chip8.hpp"
+#include <iostream>
 
-int main(void) {
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "Failed to run: 2 arguments needed.\n";
+        std::cout << "Help:\n";
+        std::cout << "    ./achip8emu <file_path>\n";
+    }
+    
     auto chip8 = Chip8();
-    chip8.load("./2-ibm-logo.ch8");
+    chip8.load(argv[1]);
     chip8.run();
     return 0;
 }
