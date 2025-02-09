6c2c461f342b9d9ac3345126084caeec453a3da7
Anderson Felippe <adfelippe@gmail.com>
2025-02-09 21:58:45 +0100

    Add Keyboard interface

---
+++ src/IKeyboard.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+class IKeyboard {
+public:
+    IKeyboard() {}
+    virtual ~IKeyboard() {}
+
+    struct Key {
+        enum class State {
+            kReleased = 1,
+            kPressed = 0
+        };
+
+        State state;
+        char value;
+    };
+
+    virtual Key getKey(void) = 0;
+};
